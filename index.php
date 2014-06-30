1a2b116e50a768922eec08c5c6dd684bfc800a31
Konstantinos Ntemagkos <kntemagkos@gmail.com>
2014-06-30 22:03:17 +0300

    Index: basic layout and css styling
    * Adds basic elements from mockups.

---
+++ index.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+    <title>Eudoxus home</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+</head>
+<body bgcolor="#FFFFFF" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
+
+    <div id="header" >
+        <div><img style="float: left;" src="img/logo.png"></div>
+        <div style="display: block">
+            <form>
+                    <input class="search rounded" type="text" placeholder="αναζήτηση συγγραμμάτων..." required>
+                    <input id="search-button" type="button" value="">
+            </form>
+        </div>
+        <div id=user>
+            <div id="username">guest</div>
+            <img style="float: right;" src="img/user-icon.png">
+        </div>
+    </div>
+
+    <div id="mainmenu" ></div>
+    <div id="subtitle" ></div>
+    <div id="content"></div>
+    <div id="footer"></div>
+
+</body>
+</html>
