10a2f19ef08f9315b58be03eb7624fc2948eb8db
Konstantinos Ntemagkos <kntemagkos@gmail.com>
2014-07-11 17:14:42 +0300

    index: homepage styling finalization
    * Addition of index icons and text.
    
    * TODO: Icons for professors and publishers. Explanatory text.
    
    * TODO: Fix search bar becoming red on focus out. Advanced search button.

--- index.php
+++ index.php
@@ -2,28 +2,284 @@
 <head>
     <title>Eudoxus home</title>
     <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 </head>
 <body bgcolor="#FFFFFF" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
+    <div class="container-fluid">
+            <div id="header" class="row">
+                <div class="col-md-2"><img src="img/logo.png"></div>
+                <div class="col-md-7">
+                    <form>
+                            <input class="search rounded" type="text" placeholder="αναζήτηση συγγραμμάτων..." required>
+                            <input id="search-button" type="button" value="">
+                    </form>
+                </div>
+                <div id=user class="col-md-3">
+                    <img style="float: right; margin-top: 22px;" src="img/down-arrow.png">
+                    <div id="username">guest</div>
+                    <img style="float: right;" src="img/user-icon.png">
+                </div>
+            </div>
 
-    <div id="header" >
-        <div><img style="float: left;" src="img/logo.png"></div>
-        <div style="display: block">
-            <form>
-                    <input class="search rounded" type="text" placeholder="αναζήτηση συγγραμμάτων..." required>
-                    <input id="search-button" type="button" value="">
-            </form>
-        </div>
-        <div id=user>
-            <div id="username">guest</div>
-            <img style="float: right;" src="img/user-icon.png">
-        </div>
+            <div id="mainmenu" class="row">
+                <div class="row text-center" >
+                    <div class="col-md-2 lead" style="font-size:16px;">Αρχική</div>
+                    <div class="col-md-2 lead" style="font-size:16px;">Ανακοινώσεις</div>
+                    <div class="col-md-2 lead" style="font-size:16px;">Επικοινωνία</div>
+                    <div class="col-md-2 lead" style="font-size:16px;">Χρήσιμα</div>
+                    <div class="col-md-2 lead" style="float:right;">
+                        <img src="img/rss-logo-big.png">
+                        <img src="img/facebook-logo-big.png">
+                        <img src="img/twitter-logo-big.png">
+                    </div>
+
+                </div>
+            </div>
+
+            <div id="subtitle" class="row blue-font">
+                <div class="col-md-12" >
+                    <div style="float:left;"><img style="height: 30;margin-top: 5px;" src="img/books.png"></div>
+                    <div style="margin-left:55px; margin-top: 5px;" class="lead"> Ηλεκτρονική Υπηρεσία Ολοκληρωμένης Διαχείρισης Συγγραμμάτων και λοιπών βοηθημάτων</div>
+                </div>
+            </div>
+
+            <div id="content" class="row">
+                <div id="homepage-general" class="row">
+
+                    <div class="col-md-3">
+                        <div class="row">
+                            <div class="col-md-4"></div>
+                            <div class="col-md-4">
+                                <img style="display: block; margin-left: auto; margin-right: auto;" src="img/books.png">
+                            </div>
+                            <div class="col-md-4"></div>
+                        </div>
+                        <div class="row blue-font lead" style="text-align:center; font-size:16px;">
+                            <span>Λίστα Συγγραμμάτων ανα Τμήμα</span>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="row">
+                            <div class="col-md-4"></div>
+                            <div class="col-md-4">
+                                <img style="display: block; margin-left: auto; margin-right: auto;" src="img/library.png">
+                            </div>
+                            <div class="col-md-4"></div>
+                        </div>
+                        <div class="row blue-font lead" style="text-align:center; font-size:16px;">
+                            <span>Βιβλιοθήκες</span>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="row">
+                            <div class="col-md-4"></div>
+                            <div class="col-md-4">
+                                <img style="display: block; margin-left: auto; margin-right: auto;" src="img/maps-logo.png">
+                            </div>
+                            <div class="col-md-4"></div>
+                        </div>
+                        <div class="row blue-font lead" style="text-align:center; font-size:16px;">
+                            <span >Σημεία Διανομής</span>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="row" style="margin-top: -15px;">
+                            <div class="col-md-4"></div>
+                            <div class="col-md-4">
+                                <img style="display: block; margin-left: auto; margin-right: auto;" src="img/faq-logo.png">
+                            </div>
+                            <div class="col-md-4"></div>
+                        </div>
+                        <div class="row blue-font lead" style="text-align:center; font-size:16px;">
+                            <span >Συχνές Ερωτήσεις/Βοήθεια</span>
+                        </div>
+                    </div>
+
+                </div>
+
+<!------------------------------ STUDENTS ------------------------------>
+
+                <div class="row" id="homepage-students">
+                    <div class="row">
+                        <div class="col-md-1"></div>
+                        <div class="col-md-10" style="height: 30px;">
+                            <div class="lead">Φοιτητές</div>
+                        </div>
+                    </div>
+
+                    <div class="row "><div class="col-md-1"></div><div class="separator col-md-8"></div></div>
+
+                    <div class="row" style="margin-top:20px;">
+                        <div class="col-md-1"></div>
+                        <div class="col-md-3">
+                            <div class="row">
+                                <img style="display: block; margin-left: auto; margin-right: auto;" src="img/books-statement-logo.png">
+                            </div>
+                            <div class="row blue-font lead" style="text-align:center; font-size:16px;">
+                                <span >Δήλωση συγγραμμάτων</span>
+                            </div>
+                            <div class="row blue-font lead" style="text-align:center; font-size:12px;">
+                                <span>Oι φοιτητές έχουν τη δυνατότητα σε κάθε εξάμηνο να εισέλθουν στο σύστημα του «Εύδοξος», να ενημερωθούν για τα Συγγράμματα που προτείνουν οι διδάσκοντες των μαθημάτων και να διαλέξουν τα Συγγράμματα που επιθυμούν.</span>
+                            </div>
+                        </div>
+                        <div class="col-md-2"></div>
+                        <div class="col-md-3">
+                            <div class="row">
+                                <img style="display: block; margin-left: auto; margin-right: auto;" src="img/books-exchange-logo.png">
+                            </div>
+                            <div class="row blue-font lead" style="text-align:center; font-size:16px;">
+                                <span>Ανταλλαγή συγγραμμάτων</span>
+                            </div>
+                            <div class="row blue-font lead" style="text-align:center; font-size:12px;">
+                                <span>Με τη νέα αυτή δράση - εφάμιλλη με αντίστοιχες πολλών Πανεπιστημίων ανά τον κόσμο - μπορείς να παραχωρήσεις ή να παραλάβεις συγγράμματα από φοιτητές του Τμήματός σου.</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+<!------------------------------ PROFESSORS ------------------------------>
+
+
+                <div class="row" id="homepage-professors">
+                    <div class="row">
+                        <div class="col-md-1"></div>
+                        <div class="col-md-10" style="height: 30px;">
+                            <div class="lead">Καθηγητές</div>
+                        </div>
+                    </div>
+
+                    <div class="row "><div class="col-md-1"></div><div class="separator col-md-8"></div></div>
+
+                    <div class="row" style="margin-top:20px;">
+                        <div class="col-md-1"></div>
+                        <div class="col-md-3">
+                            <div class="row">
+                                <img style="display: block; margin-left: auto; margin-right: auto;" src="img/books-statement-logo.png">
+                            </div>
+                            <div class="row blue-font lead" style="text-align:center; font-size:16px;">
+                                <span >Δήλωση συγγραμμάτων</span>
+                            </div>
+                            <div class="row blue-font lead" style="text-align:center; font-size:12px;">
+                                <span>Oι φοιτητές έχουν τη δυνατότητα σε κάθε εξάμηνο να εισέλθουν στο σύστημα του «Εύδοξος», να ενημερωθούν για τα Συγγράμματα που προτείνουν οι διδάσκοντες των μαθημάτων και να διαλέξουν τα Συγγράμματα που επιθυμούν.</span>
+                            </div>
+                        </div>
+                        <div class="col-md-2"></div>
+                        <div class="col-md-3">
+                            <div class="row">
+                                <img style="display: block; margin-left: auto; margin-right: auto;" src="img/books-exchange-logo.png">
+                            </div>
+                            <div class="row blue-font lead" style="text-align:center; font-size:16px;">
+                                <span>Ανταλλαγή συγγραμμάτων</span>
+                            </div>
+                            <div class="row blue-font lead" style="text-align:center; font-size:12px;">
+                                <span>Με τη νέα αυτή δράση - εφάμιλλη με αντίστοιχες πολλών Πανεπιστημίων ανά τον κόσμο - μπορείς να παραχωρήσεις ή να παραλάβεις συγγράμματα από φοιτητές του Τμήματός σου.</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+<!------------------------------ SECRETARY ------------------------------>
+
+
+                <div class="row" id="homepage-secretary">
+                    <div class="row">
+                        <div class="col-md-1"></div>
+                        <div class="col-md-10" style="height: 30px;">
+                            <div class="lead">Γραμματείες Τμημάτων</div>
+                        </div>
+                    </div>
+
+                    <div class="row "><div class="col-md-1"></div><div class="separator col-md-8"></div></div>
+
+                    <div class="row" style="margin-top:20px;">
+                        <div class="col-md-1"></div>
+                        <div class="col-md-3">
+                            <div class="row">
+                                <img style="display: block; margin-left: auto; margin-right: auto;" src="img/books-administration-logo.png">
+                            </div>
+                            <div class="row blue-font lead" style="text-align:center; font-size:16px;">
+                                <span >Διαχείριση μαθημάτων/συγγραμμάτων</span>
+                            </div>
+                            <div class="row blue-font lead" style="text-align:center; font-size:12px;">
+                                <span>Oι φοιτητές έχουν τη δυνατότητα σε κάθε εξάμηνο να εισέλθουν στο σύστημα του «Εύδοξος», να ενημερωθούν για τα Συγγράμματα που προτείνουν οι διδάσκοντες των μαθημάτων και να διαλέξουν τα Συγγράμματα που επιθυμούν.</span>
+                            </div>
+                        </div>
+                        <div class="col-md-2"></div>
+                        <div class="col-md-3">
+                            <div class="row">
+                                <img style="display: block; margin-left: auto; margin-right: auto;" src="img/quick-transfer-logo.png">
+                            </div>
+                            <div class="row blue-font lead" style="text-align:center; font-size:16px;">
+                                <span>Υπηρεσία Ταχυμεταφοράς</span>
+                            </div>
+                            <div class="row blue-font lead" style="text-align:center; font-size:12px;">
+                                <span>Με τη νέα αυτή δράση - εφάμιλλη με αντίστοιχες πολλών Πανεπιστημίων ανά τον κόσμο - μπορείς να παραχωρήσεις ή να παραλάβεις συγγράμματα από φοιτητές του Τμήματός σου.</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+<!------------------------------ PUBLISHERS ------------------------------>
+
+                <div class="row" id="homepage-publishers">
+                    <div class="row">
+                        <div class="col-md-1"></div>
+                        <div class="col-md-10" style="height: 30px;">
+                            <div class="lead">Εκδότες</div>
+                        </div>
+                    </div>
+
+                    <div class="row "><div class="col-md-1"></div><div class="separator col-md-8"></div></div>
+
+                    <div class="row" style="margin-top:20px;">
+                        <div class="col-md-1"></div>
+                        <div class="col-md-3">
+                            <div class="row">
+                                <img style="display: block; margin-left: auto; margin-right: auto;" src="img/books-statement-logo.png">
+                            </div>
+                            <div class="row blue-font lead" style="text-align:center; font-size:16px;">
+                                <span >Δήλωση συγγραμμάτων</span>
+                            </div>
+                            <div class="row blue-font lead" style="text-align:center; font-size:12px;">
+                                <span>Oι φοιτητές έχουν τη δυνατότητα σε κάθε εξάμηνο να εισέλθουν στο σύστημα του «Εύδοξος», να ενημερωθούν για τα Συγγράμματα που προτείνουν οι διδάσκοντες των μαθημάτων και να διαλέξουν τα Συγγράμματα που επιθυμούν.</span>
+                            </div>
+                        </div>
+                        <div class="col-md-2"></div>
+                        <div class="col-md-3">
+                            <div class="row">
+                                <img style="display: block; margin-left: auto; margin-right: auto;" src="img/books-exchange-logo.png">
+                            </div>
+                            <div class="row blue-font lead" style="text-align:center; font-size:16px;">
+                                <span>Ανταλλαγή συγγραμμάτων</span>
+                            </div>
+                            <div class="row blue-font lead" style="text-align:center; font-size:12px;">
+                                <span>Με τη νέα αυτή δράση - εφάμιλλη με αντίστοιχες πολλών Πανεπιστημίων ανά τον κόσμο - μπορείς να παραχωρήσεις ή να παραλάβεις συγγράμματα από φοιτητές του Τμήματός σου.</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+
+
+            </div>
+
+            <div id="footer" class="row">
+                <div class="col-md-6 lead" style="color:white; font-size:14px;">© Υπουργείο Παιδείας και Θρησκευμάτων | Ελληνική Δημοκρατία
+                                      <br>Με τη συγχρηματοδότηση της Ελλάδας & της Ευρωπαϊκής Ένωσης</div>
+                <div class="col-md-4"></div>
+                <div class="col-md-2">
+                    <img src="img/facebook-logo-big.png">
+                    <img src="img/twitter-logo-big.png">
+                </div>
+            </div>
     </div>
-
-    <div id="mainmenu" ></div>
-    <div id="subtitle" ></div>
-    <div id="content"></div>
-    <div id="footer"></div>
-
 </body>
 </html>
