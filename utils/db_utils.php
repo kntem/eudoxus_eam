511804967dc31f8a942092ef92f943ea2b4c5179
Konstantinos Ntemagkos <kntemagkos@gmail.com>
2014-07-16 19:10:13 +0300

    db_utils: book abstract addition

--- utils/db_utils.php
+++ utils/db_utils.php
@@ -16,6 +16,7 @@ class Book {
 	private $location;
 	private $dimensions;
 	private $pages;
+	private $abstract;
 
     function __construct() {
         // Gets book from db and calls set_book
@@ -24,7 +25,7 @@ class Book {
 
     function set_book($id, $type, $cover_image, $authors, $edition, $isbn,
                       $publisher, $webpage, $location, $cover, $dimensions,
-                      $pages){
+                      $pages, $abstract){
         $this->id = $id;
         $this->type= $type;
         $this->cover_image = $cover_image;
@@ -34,41 +35,56 @@ class Book {
         $this->webpage = $location;
         $this->cover = $dimensions;
         $this->pages = $pages;
+        $this->abstract= $abstract;
     }
 
 
     function get_id(){
         return $this->id;
     }
+
     function get_type(){
         return $this->type;
     }
+
     function get_cover_image(){
         return $this->cover_image;
     }
+
     function get_authors(){
         return $this->authors;
     }
+
     function get_edition(){
         return $this->edition;
     }
+
     function get_isbn(){
         return $this->edition;
     }
+
     function get_publisher(){
         return $this->publisher;
     }
+
     function get_webpage(){
         return $this->webpage;
     }
+
     function get_location(){
         return $this->location;
     }
+
     function get_dimensions(){
         return $this->dimensions;
     }
+
     function get_pages(){
         return $this->pages;
+    }
+
+    function get_abstract(){
+        return $this->abstract;
     }
 }
 
