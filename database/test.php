d55d8865e80087f50d5428c38cc6c87065336f4e
blenikos <blenikos@gmail.com>
2014-07-20 02:50:35 +0300

    New tests added
    *Changed test.php adding new tests for login and register functions

--- database/test.php
+++ database/test.php
@@ -1,20 +1,23 @@
 <?php
 
-//include 'utils/db_link.php';
-include 'utils/db_utils.php';
+include 'utils/db_link.php';
+include 'utils/user_utils.php';
 
- $Linker = new DBLink();          
- session_start();
- $_SESSION['link']=$Linker;
-        
- $success = $Linker->DBLinking();
+Global $Linker;
+
+$Linker= new DBLink();                 
+$success = $Linker->DBLinking();
 if($success == false) printf("CONNECTION ERROR!!! NO DATABASE AVAILABLE!!!");
 else printf("CONNECTION SUCCESSFULL!!!");
 
-$USER = new Student(1);
+
+
+$USER = new User();
+
+$USER->register("blenikos","abcd17","Professors","std10179@di.uoa.gr");
 
 printf("USER CREATION SUCCESSFULL!!!");
 
-printf($USER->get_name());
+//printf($USER->get_user_type());
 
 ?>
